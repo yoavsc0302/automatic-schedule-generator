05c92aef046541bb9d3a2cd54ec27f0c5c7b5eb5
Yoav Schwammenthal <yoavsc0302@gmail.com>
2020-12-28 00:15:46 +0200

    In the function 'add_new_person', the sum of the new person that will be added to the sheets is the average of all the people of his kind in the sheet unless he is the first one which in that case, his sum will be 0

--- excel_modifications.py
+++ excel_modifications.py
@@ -5,12 +5,22 @@ import numpy as np
 import openpyxl
 import xlsxwriter
 import pickle
-# files_location_df = pd.read_csv('files_locations.csv')
-# ilutzim_file = files_location_df['ilutzim'][0]
-# justice_board_file = files_location_df['justice_board'][0]
+import math
 
 # Show entire df when printed
 pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+# Read every sheet in the justice board file and make a df out of it
+makel_officer_df = pd.read_excel('justice_board.xlsx',
+                                 sheet_name='Makel Officer',
+                                  engine='openpyxl', index_col=0)
+makel_operator_df = pd.read_excel('justice_board.xlsx',
+                                  sheet_name='Makel Operator',
+                                  engine='openpyxl', index_col=0)
+manager_df = pd.read_excel('justice_board.xlsx', sheet_name='Manager',
+                           engine='openpyxl', index_col=0)
+samba_df = pd.read_excel('justice_board.xlsx', sheet_name='Samba',
+                         engine='openpyxl', index_col=0)
 
 
 def create_ilutzim_excel(makel_names, manager_names, samba_names):
@@ -52,6 +62,243 @@ def create_ilutzim_excel(makel_names, manager_names, samba_names):
     # Close the Pandas Excel writer and output the Excel file.
     writer.save()
 
+
+def create_justice_board_excel():
+    """
+    Create the ilutzim excel file as a 'Multiply indexed DataFrame'
+    source:https://jakevdp.github.io/PythonDataScienceHandbook/03.05-
+    hierarchical-indexing.html for each population
+    :param makel_names: list that contains the names of every 'makel'
+    :param manager_names: list that contains the names of every 'manaager'
+    :param samba_names: list that contains the names of every 'samab'
+    """
+
+    #Makel officer df:
+    columns = ['Name', 'Sum']
+    makel_officer_df = pd.DataFrame(columns=columns)
+
+    # Makel operator df:
+    makel_operate_df = pd.DataFrame(columns=columns)
+
+    # Manager df:
+    manager_df = pd.DataFrame(columns=columns)
+
+    # Samba df:
+    columns = ['Name', 'Sum', 'Samba', 'Fast caller and Toran']
+    samba_df = pd.DataFrame(columns=columns)
+
+    # Create a Pandas Excel writer using XlsxWriter as the engine.
+    writer = pd.ExcelWriter('justice_board.xlsx', engine='xlsxwriter')
+
+    # Write each dataframe to a different worksheet.
+    makel_officer_df.to_excel(writer, sheet_name='Makel Officer')
+    makel_operate_df.to_excel(writer, sheet_name='Makel Operator')
+    manager_df.to_excel(writer, sheet_name='Manager')
+    samba_df.to_excel(writer, sheet_name='Samba')
+
+    # Close the Pandas Excel writer and output the Excel file.
+    writer.save()
+
+
 def create_file_location_csv():
-    files_location_df = pd.DataFrame({'ilutzim':['i location'],'justice_board':['jb location']})
+    """
+    Create csv thats stores the ilutzim and the justice board files locations'
+    """
+    files_location_df = pd.DataFrame({'ilutzim': ['i location'],
+                                      'justice_board': ['jb location']})
     files_location = files_location_df.to_csv('files_location.csv')
+
+
+def add_new_person(name, manager_var, makel_officer_var, makel_operator_var,
+                   samba_var, fast_and_toran_var, warning_label):
+    """
+    Add a new person to the right sheets according to the jobs he can do:
+    manager/makel officer/makel opperator/samba/fast and toran
+    :param name: the name of the new person
+    :param manager_var: equals 1 if the manager job checkbox is checked
+    :param makel_officer_var: equals 1 if the makel officer job checkbox
+     is checked
+    :param makel_operator_var: equals 1 if the makel operator job checkbox
+     is checked
+    :param samba_var: equals 1 if the samba job checkbox is checked
+    :param fast_and_toran_var: equals 1 if the fast and toran job checkbox
+     is checked
+    :param warning_label: the warning label in the window that it's text
+     will apperat in case of some kind of error
+    """
+    try:
+        # Create a Pandas Excel writer using XlsxWriter as the engine.
+        with pd.ExcelWriter('justice_board.xlsx', engine='openpyxl', mode='a')\
+                as writer:
+            workbook = writer.book
+
+        # Read each sheet in the justice board file and make a df out of iy
+        makel_officer_df = pd.read_excel('justice_board.xlsx',
+                                         sheet_name='Makel Officer',
+                                         engine='openpyxl', index_col=0)
+        makel_operator_df = pd.read_excel('justice_board.xlsx',
+                                          sheet_name='Makel Operator',
+                                          engine='openpyxl', index_col=0)
+        manager_df = pd.read_excel('justice_board.xlsx',
+                                   sheet_name='Manager',
+                                   engine='openpyxl', index_col=0)
+        samba_df = pd.read_excel('justice_board.xlsx',
+                                 sheet_name='Samba',
+                                 engine='openpyxl', index_col=0)
+
+        # If the new person is a 'makel officer', insert his name into
+        # the 'makel officer' sheet and set his sum to the average of
+        # everybodies' sum
+        if makel_officer_var.get() == 1:
+            workbook.remove(workbook['Makel Officer'])
+            try:
+                sum_to_be_set = math.floor(makel_officer_df['Sum'].mean())
+            except: # If this is the first person in the sheet
+                sum_to_be_set = 0
+            makel_officer_df = makel_officer_df.append({'Name':name,
+                                                        'Sum': sum_to_be_set},
+                                                       ignore_index=True)
+            # Write dataframe to the worksheet.
+            makel_officer_df.to_excel(writer, sheet_name='Makel Officer')
+
+        # If the new person is a 'makel operator', insert his name into
+        # the 'makel operator' sheet and set his sum to the average of
+        # everybodies' sum
+        if makel_operator_var.get() == 1:
+            workbook.remove(workbook['Makel Operator'])
+            try:
+                sum_to_be_set = math.floor(makel_operator_df['Sum'].mean())
+            except: # If this is the first person in the sheet
+                sum_to_be_set = 0
+            makel_operator_df = makel_operator_df.append({'Name':name,
+                                                          'Sum': sum_to_be_set},
+                                                         ignore_index=True)
+            # Write dataframe to the worksheet.
+            makel_operator_df.to_excel(writer, sheet_name='Makel Operator')
+
+        # If the new person is a 'manager', insert his name into
+        # the 'manager' sheet and set his sum to the average of everybodies' sum
+        if manager_var.get() == 1:
+            workbook.remove(workbook['Manager'])
+            try:
+                sum_to_be_set = math.floor(manager_df['Sum'].mean())
+            except: # If this is the first person in the sheet
+                sum_to_be_set = 0
+            manager_df = manager_df.append({'Name':name, 'Sum': sum_to_be_set},
+                                           ignore_index=True)
+            # Write dataframe to the worksheet.
+            manager_df.to_excel(writer, sheet_name='Manager')
+
+        # If the new person is either a 'samba' or 'fast and toran',
+        # insert his name into the 'samba' sheet and specify in the 'samba' and
+        # 'fast and toran' columns what he is by 'TRUE' and 'False' values
+        # Toran and Toran+Samba share the same mean
+        if fast_and_toran_var.get() == 1:
+            try:
+                sum_to_be_set = math.floor(samba_df
+                                           [samba_df['Fast caller and Toran']
+                                            == True]['Sum'].mean())
+            except: # If this is the first person of this kind in the sheet
+                sum_to_be_set = 0
+            if samba_var.get() == 0:
+                samba_df = samba_df.append({'Name':name, 'Sum': sum_to_be_set,
+                                            'Samba': False,
+                                            'Fast caller and Toran': True},
+                                           ignore_index=True)
+            else:
+                samba_df = samba_df.append({'Name':name, 'Sum': sum_to_be_set,
+                                            'Samba': True,
+                                            'Fast caller and Toran': True},
+                                           ignore_index=True)
+            # Write dataframe to the worksheet.
+            workbook.remove(workbook['Samba'])
+            samba_df.to_excel(writer, sheet_name='Samba')
+        else:
+            if samba_var.get() == 1:
+                try:
+                    sum_to_be_set = math.floor(
+                        samba_df[(samba_df['Fast caller and Toran'] == False)
+                                 & (samba_df['Samba'] == True)]['Sum'].mean())
+                except: # If this is the first person of this kind in the sheet
+                    sum_to_be_set = 0
+                samba_df = samba_df.append(
+                    {'Name': name, 'Sum': sum_to_be_set, 'Samba': True,
+                     'Fast caller and Toran': False}, ignore_index=True)
+                workbook.remove(workbook['Samba'])
+                samba_df.to_excel(writer, sheet_name='Samba')
+        # Save the justice board file
+        writer.save()
+        warning_label['text'] = ''
+        warning_label['bg'] = None
+
+    except:
+
+        # Show a warning in the edit people window
+        warning_label['text'] = 'אזהרה: הקובץ של \nלוח הצדק פתוח.\n אנא סגור ' \
+                                'אותו \nכדי שיתאפשר \nלשמור את השינויים!'
+        warning_label['bg'] = 'red'
+
+
+def get_list_of_all_people():
+    """
+    Go over all sheets in the justice board file, grab all names and remove
+    duplicated names
+    :return: list of names in the justice board file
+    """
+    list_of_all_df = [makel_officer_df, makel_operator_df, manager_df, samba_df]
+    names_of_all_people = []
+    for df in list_of_all_df:
+        names_of_all_people += df['Name'].values.tolist()
+    names_of_all_people = list(set(names_of_all_people))
+    return names_of_all_people
+
+
+def delete_person(name_of_person, warning_label):
+    """
+    Delete the given person from every sheet in the justice board file
+    :param name_of_person: the person to delete
+    :param warning_label: a label that will warn the user if the justice
+     board file is open
+    :return:
+    """
+    try:
+        # Create a Pandas Excel writer using XlsxWriter as the engine.
+        with pd.ExcelWriter('justice_board.xlsx', engine='openpyxl',
+                            mode='a') as writer:
+            workbook = writer.book
+
+        makel_officer_df = pd.read_excel('justice_board.xlsx',
+                                         sheet_name='Makel Officer',
+                                         engine='openpyxl', index_col=0)
+        makel_operator_df = pd.read_excel('justice_board.xlsx',
+                                          sheet_name='Makel Operator',
+                                          engine='openpyxl', index_col=0)
+        manager_df = pd.read_excel('justice_board.xlsx',
+                                   sheet_name='Manager',
+                                   engine='openpyxl', index_col=0)
+        samba_df = pd.read_excel('justice_board.xlsx', sheet_name='Samba',
+                                 engine='openpyxl', index_col=0)
+
+        dict_of_df = {'Makel Officer': makel_officer_df,
+                      'Makel Operator': makel_operator_df,
+                      'Manager': manager_df, 'Samba': samba_df}
+
+        # Run over each DF and delete the person from it if the person is in it
+        # and update the sheet
+        for key in dict_of_df:
+            df = dict_of_df[key]
+            if name_of_person in df['Name'].values:
+                index_of_name_to_remove = df[df['Name'] == name_of_person].index
+                removed_name_df = df.drop(index_of_name_to_remove)\
+                    .reset_index(drop=True)
+                workbook.remove(workbook[key])
+                removed_name_df.to_excel(writer, sheet_name=key)
+
+        writer.save()
+        warning_label['text'] = ''
+        warning_label['bg'] = None
+
+    except:
+        warning_label['text'] = 'אזהרה: הקובץ של \nלוח הצדק פתוח.\n אנא סגור ' \
+                                'Iאותו \nכדי שיתאפשר \nלשמור את השינויים!'
+        warning_label['bg'] = 'red'
--- gui.py
+++ gui.py
@@ -1,6 +1,17 @@
 # Create the gui for the app
 import tkinter as tk
 import pandas as pd
+import webbrowser
+import excel_modifications as em
+
+
+def open_url(url):
+    """
+    Opens the given url
+    :param url: the url which the function will open
+    """
+    webbrowser.open(url)
+
 
 def get_ilutzim_location():
     """
@@ -11,6 +22,7 @@ def get_ilutzim_location():
     ilutzim_file = files_location_df['ilutzim'][0]
     return ilutzim_file
 
+
 def get_justice_board_location():
     """
     Get the justice board file location
@@ -19,6 +31,7 @@ def get_justice_board_location():
     files_location_df = pd.read_csv('files_location.csv')
     justice_board_file = files_location_df['justice_board'][0]
     return justice_board_file
+
 
 def save_files_new_locations(files_location_df, ilutzim_entry, justice_entry):
     """
@@ -34,12 +47,7 @@ def save_files_new_locations(files_location_df, ilutzim_entry, justice_entry):
     files_location_df.to_csv('files_location.csv', index=False)
 
 
-# Create a main window and define it's size
-window = tk.Tk()
-window.geometry("700x600")
-window.title('משבץ צוות כונן אוטומטי')
-
-def openEditPeopleWindow():
+def open_edit_people_window():
     """
     Create a new window for editing people
     """
@@ -66,36 +74,78 @@ def openEditPeopleWindow():
     new_person_headline.config(font=("calibri", 12))
 
     # Delete a person title
-    delete_person_healine = tk.Label(master=edit_people_window, text="מחק אדם")
-    delete_person_healine.grid(row=0, column=1, sticky="nsew")
-    delete_person_healine.config(font=("calibri", 12))
+    delete_person_headline = tk.Label(master=edit_people_window, text="מחק אדם")
+    delete_person_headline.grid(row=0, column=1, sticky="nsew")
+    delete_person_headline.config(font=("calibri", 12))
 
     # Get the name of the new person
-    name = tk.Entry(edit_people_window).grid(row=1, column=5)
+    name = tk.Entry(edit_people_window)
+    name.grid(row=1, column=5)
 
-    # Get person rolls
-    manager = tk.Checkbutton(edit_people_window, text="מנהל").grid(row=2, column=5)
-    makel_officer = tk.Checkbutton(edit_people_window, text="קצין מקל").grid(row=3, column=5)
-    makel_operator = tk.Checkbutton(edit_people_window, text="מפעיל מקל").grid(row=4, column=5)
-    samba = tk.Checkbutton(edit_people_window, text="סמבצ").grid(row=2, column=4)
-    fast_caller = tk.Checkbutton(edit_people_window, text="קריאה מהירה").grid(row=3, column=4)
-    toran_unit = tk.Checkbutton(edit_people_window, text="ת. יחידתי").grid(row=4, column=4)
+    # Get person rolls from checkboxes
+    manager_var = tk.IntVar()
+    makel_officer_var = tk.IntVar()
+    makel_operator_var = tk.IntVar()
+    samba_var = tk.IntVar()
+    fast_and_toran_var = tk.IntVar()
 
-    #list of people
-    list_of_people = ["Jan","Feb","Mar"]
-    variable = tk.StringVar(edit_people_window)
-    variable.set(list_of_people[0])  # default value
-    w = tk.OptionMenu(edit_people_window, variable, *list_of_people).grid(row=1, column=1)
+    manager = tk.Checkbutton(edit_people_window, text="מנהל",
+                             variable=manager_var).grid(
+        row=2, column=5, sticky="e")
+    makel_officer = tk.Checkbutton(edit_people_window,
+                                   text="קצין מקל",
+                                   variable=makel_officer_var) \
+        .grid(row=3, column=5, sticky="e")
+    makel_operator = tk.Checkbutton(edit_people_window, text="מפעיל מקל",
+                                    variable=makel_operator_var) \
+        .grid(row=4, column=5, sticky="e")
+    samba = tk.Checkbutton(edit_people_window, text="סמבצ",
+                           variable=samba_var).grid(row=2, column=4, sticky="e")
+    fast_caller_and_toran = tk.Checkbutton(edit_people_window,
+                                           text="ת. יחידתי + ק.מ",
+                                           variable=fast_and_toran_var) \
+        .grid(row=3, column=4, sticky="e")
+
+    # list of people
+    list_of_people = em.get_list_of_all_people()
+    list_if_empty = ['The file is empty']
+    chosen_option = tk.StringVar(edit_people_window)
+
+    try:
+        chosen_option.set(list_of_people[0])  # default value
+        dropped_down_menu = tk.OptionMenu(edit_people_window, chosen_option,
+                                          *list_of_people)
+    except:
+        chosen_option.set(list_if_empty[0])  # If the file is empty
+        dropped_down_menu = tk.OptionMenu(edit_people_window, chosen_option,
+                                          *list_if_empty)
+
+    dropped_down_menu.grid(row=1, column=1)
+
+    # Add a warning alert
+    warning_label = tk.Label(edit_people_window, text='')
+    warning_label.grid(row=2, column=1)
 
     # Add person button
-    add_person = tk.Button(edit_people_window, text="הוסף בן אדם", bg="blue")
+    add_person = tk.Button(edit_people_window, text="הוסף בן אדם", bg="blue",
+                           command=lambda: em.add_new_person(name.get(),
+                                                             manager_var,
+                                                             makel_officer_var,
+                                                             makel_operator_var,
+                                                             samba_var,
+                                                             fast_and_toran_var,
+                                                             warning_label))
     add_person.grid(row=5, column=5, sticky="nsew")
 
     # Delete person button
-    add_person = tk.Button(edit_people_window, text="מחק בן אדם", bg="blue")
-    add_person.grid(row=5, column=1, sticky="nsew")
+    delete_person = tk.Button(edit_people_window, text="מחק בן אדם",
+                              bg="blue",
+                              command=lambda: em.delete_person(
+                                  chosen_option.get(), warning_label))
+    delete_person.grid(row=5, column=1, sticky="nsew")
 
-def openChangeFileLocWindows():
+
+def open_change_file_loc_window():
     """
     Create a new window for changing ilutzim and justice board files locations'
     """
@@ -118,7 +168,8 @@ def openChangeFileLocWindows():
             frame.grid(row=i, column=j, sticky="nsew")
 
     # Justice board headline
-    justice_board_headline = tk.Label(master=change_file_loc_windows, text="לוח צדק")
+    justice_board_headline = tk.Label(master=change_file_loc_windows,
+                                      text="לוח צדק")
     justice_board_headline.grid(row=0, column=1, sticky="nsew")
     justice_board_headline.config(font=("calibri", 12))
 
@@ -138,12 +189,23 @@ def openChangeFileLocWindows():
     ilutzim_file_loc.insert(0, get_ilutzim_location())
 
     # Save files locations button
-    save_files_locations = tk.Button(change_file_loc_windows, text="שמור מיקומים", bg="blue", command=lambda: save_files_new_locations(pd.read_csv('files_location.csv'),ilutzim_file_loc,justice_board_file_loc))
+    save_files_locations = tk.Button(change_file_loc_windows,
+                                     text="שמור מיקומים", bg="blue",
+                                     command=lambda:
+                                     save_files_new_locations(
+                                         pd.read_csv('files_location.csv'),
+                                         ilutzim_file_loc,
+                                         justice_board_file_loc))
     save_files_locations.grid(row=3, column=1, sticky="nsew")
 
 
+# Create the homepage window and define it's size
+window = tk.Tk()
+window.geometry("550x450")
+window.title('משבץ צוות כונן אוטומטי')
+
 # Divide the windows into 7x7 frames
-for i in range(7):
+for i in range(6):
     window.columnconfigure(i, weight=1, minsize=75)
     window.rowconfigure(i, weight=1, minsize=50)
     for j in range(7):
@@ -152,7 +214,7 @@ for i in range(7):
             relief=tk.RAISED,
             borderwidth=1,
         )
-        frame.grid(row=i, column=j,sticky="nsew")
+        frame.grid(row=i, column=j, sticky="nsew")
 
 # Set window's headline
 window_headline = tk.Label(master=window, text="דף הבית")
@@ -161,40 +223,39 @@ window_headline.config(font=("calibri", 20))
 
 # Set buttons
 
-#all_button
-generate_all = tk.Button(text="שבץ צוות שלם",bg="blue")
+# all_button
+generate_all = tk.Button(text="שבץ צוות שלם", bg="blue")
 generate_all.grid(row=2, column=3, sticky="nsew")
 
-#makel_button
-generate_makel = tk.Button(text="מקל",bg="blue")
+# makel_button
+generate_makel = tk.Button(text="מקל", bg="blue")
 generate_makel.grid(row=3, column=4, sticky="nsew")
 
-#manager_button
-generate_manager = tk.Button(text="מנהלים",bg="blue")
+# manager_button
+generate_manager = tk.Button(text="מנהלים", bg="blue")
 generate_manager.grid(row=3, column=3, sticky="nsew")
 
-#samba_button
-generate_samba = tk.Button(text="סמבץ/ק.מ/ת.י",bg="blue")
+# samba_button
+generate_samba = tk.Button(text="סמבץ/ק.מ/ת.י", bg="blue")
 generate_samba.grid(row=3, column=2, sticky="nsew")
 
-#go to files location button
-go_to_files_loc = tk.Button(text="שנה קבצים", bg="blue", command=openChangeFileLocWindows)
+# go to files location button
+go_to_files_loc = tk.Button(text="שנה קבצים", bg="blue",
+                            command=open_change_file_loc_window)
 go_to_files_loc.grid(row=0, column=6, sticky="nsew")
 
-#go to edit_people_button
-go_to_edit_people = tk.Button(text="ערוך אנשים", bg="blue", command=openEditPeopleWindow)
+# go to edit_people_button
+go_to_edit_people = tk.Button(text="ערוך אנשים", bg="blue",
+                              command=open_edit_people_window)
 go_to_edit_people.grid(row=1, column=6, sticky="nsew")
 
-#open ilutzim file button
-open_ilutzim = tk.Button(text="פתח אילוצים",bg="blue")
+# open ilutzim file button
+open_ilutzim = tk.Button(text="פתח אילוצים", bg="blue",
+                         command=lambda: open(get_ilutzim_location()))
 open_ilutzim.grid(row=0, column=0, sticky="nsew")
 
-#open justice board file button
-open_justice_board = tk.Button(text="פתח לוח צדק",bg="blue")
+# open justice board file button
+open_justice_board = tk.Button(text="פתח לוח צדק", bg="blue")
 open_justice_board.grid(row=1, column=0, sticky="nsew")
 
-
-
-
 window.mainloop()
-
